2ab35545573f66e931224d7f88256a669a3ead86
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-04-08 08:27:08 -0700

    quick bugfix

--- projects/n-gon bookmarklets/deBroglieBohmTheory.js
+++ projects/n-gon bookmarklets/deBroglieBohmTheory.js
@@ -1,3 +1,18 @@
+Math.PHI = Math.sqrt(1.25) + 0.5
+
+String.prototype.shuffle = function () { //scrambles text
+  var a = this.split(""),
+    n = a.length;
+
+  for (var i = n - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+  }
+  return a.join("");
+}
+
 simulation.newEphemeraID = () => {
     let key = 16384 * Math.PHI * Math.pow(Math.PI, Math.E);
     let tag = key / Math.random();
