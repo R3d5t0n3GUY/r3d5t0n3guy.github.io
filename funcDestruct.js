6a959cdd27a18d37a534d4e19b54070d22dc2491
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-04-19 21:33:39 -0700

    let funcDestruct = new Project()...

---
+++ funcDestruct.js
@@ -0,0 +1,9 @@
+/*
+  JavaScript Code Parser
+  Reads inputted string as set of functions
+  Parses result as an array
+  My first REAL JS project outside of n-mod
+  Not sure how I'm going to do this...
+
+  -R3d5t0n3_GUY
+*/
