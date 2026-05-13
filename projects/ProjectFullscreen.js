18b2abab1306626ec1d3160471dfe05b3283dd36
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-05-13 16:32:39 -0700

    New TW Ext

---
+++ projects/ProjectFullscreen.js
@@ -0,0 +1,57 @@
+// Name: Project Fullscreen
+// ID: r3d5t0n3guyprojectfullscreen
+// Description: Enter and exit the built-in fullscreen
+// By: R3d5t0n3_GUY
+
+(function (Scratch) {
+  'use strict';
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error('This extension must run unsandboxed');
+  }
+  
+  class ProjectFullscreen {
+    getInfo() {
+      return {
+        id: 'ProjectFullscreen',
+        name: 'Project Fullscreen',
+        color1: '#1FBF5F',
+        blocks: [
+          {
+            opcode: 'enterFullscreen',
+            func: 'enter',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'enter fullscreen',
+          },
+          {
+            opcode: 'exitFullscreen',
+            func: 'exit',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'exit fullscreen',
+          },
+          {
+            opcode: 'evaluateFullscreen',
+            func: 'evaluate',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'is fullscreen?'
+          },
+        ]
+      };
+    }
+    enter() {
+      if (document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length > 1) {
+        document.getElementsByClassName('stage-header_stage-button-icon_3zzFK')[document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length - 1].parentElement.parentElement.click()
+      }
+    }
+    exit() {
+      if (document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length < 2) {
+        document.getElementsByClassName('stage-header_stage-button-icon_3zzFK')[document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length - 1].parentElement.parentElement.click()
+      }
+    }
+    evaluate() {
+      return document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length < 2
+    }
+  }
+
+  Scratch.extensions.register(new ProjectFullscreen());
+})(Scratch);
