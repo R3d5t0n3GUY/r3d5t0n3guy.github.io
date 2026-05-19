5f25de2e64e835ba54ea60ca5428a9d16a27af50
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-05-18 20:45:46 -0700

    optimization

--- projects/ProjectFullscreen.js
+++ projects/ProjectFullscreen.js
@@ -18,18 +18,6 @@
         color1: '#1FBF5F',
         blocks: [
           {
-            opcode: 'enterFullscreen',
-            func: 'enter',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'enter fullscreen',
-          },
-          {
-            opcode: 'exitFullscreen',
-            func: 'exit',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'exit fullscreen',
-          },
-          {
             opcode: 'alterFullscreen',
             func: 'switchMode',
             blockType: Scratch.BlockType.COMMAND,
@@ -51,36 +39,27 @@
           },
         ],
         menus: {
-            fullScreenActionMenu: {
-              items: [
-                {
-                  text: Scratch.translate('enter'),
-                  value: 'enterFullscreen'
-                },
-                {
-                  text: Scratch.translate('exit'),
-                  value: 'exitFullscreen'
-                },
-                {
-                  text: Scratch.translate('toggle'),
-                  value: 'toggleFullscreen'
-                },
-              ]
-            }
+          fullScreenActionMenu: {
+            items: [
+              {
+                text: Scratch.translate('enter'),
+                value: 'enterFullscreen'
+              },
+              {
+                text: Scratch.translate('exit'),
+                value: 'exitFullscreen'
+              },
+              {
+                text: Scratch.translate('toggle'),
+                value: 'toggleFullscreen'
+              },
+            ]
+          }
         }
       };
     }
     switchMode({mode}) {
-      if ((this.evaluate()) === (mode === "exitFullscreen") || mode === "toggleFullscreen") this.toggle()
-    }
-    enter() {
-      if (!this.evaluate()) this.toggle()
-    }
-    exit() {
-      if (this.evaluate()) this.toggle()
-    }
-    toggle() {
-      document.getElementsByClassName('stage-header_stage-button-icon_3zzFK')[document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length - 1].click()
+      if ((this.evaluate()) === (mode === "exitFullscreen") || mode === "toggleFullscreen") document.getElementsByClassName('stage-header_stage-button-icon_3zzFK')[document.getElementsByClassName('stage-header_stage-button-icon_3zzFK').length - 1].click()
     }
     evaluate() {
       return location.pathname.search(/\/fullscreen/i) > -1
