0e09eac28b52b260d9bad3b2ad8ab455a0d260a1
R3d5t0n3GUY <222323123+R3d5t0n3GUY@users.noreply.github.com>
2026-08-03 21:46:04 -0700

    comment mitosis

--- projects/Editable_20x4_I2C_Display.ino
+++ projects/Editable_20x4_I2C_Display.ino
@@ -108,7 +108,6 @@ byte backSlash[8] = {
 }; //handle backslash '\' printing
 
 //ASCII Character dictionary for LCD and String var "text" to use
-//ASCII Character dictionary for LCD and String var "text" to use
 char ASCII[] = {'a','A','b','B','c','C','d','D','e','E','f','F',
                 'g','G','h','H','i','I','j','J','k','K','l','L',
                 'm','M','n','N','o','O','p','P','q','Q','r','R',
